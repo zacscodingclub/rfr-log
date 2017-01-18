f6073f8c413d1c904755324a8c90422de9e11c6b
zacscodingclub <zbaston@gmail.com>
2017-01-17 21:13:30 -0500

    lots of updates

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe PagesController, type: :controller do
+RSpec.describe PagesController, type: :feature do
+  describe "GET #home" do
+    it 'responds successfully with an HTTP 200 status code' do
+      get :home
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
 
+    it 'to have specific content "Herro World"' do
+      visit root_path
+      expect(page).to have_text("Herro World")
+    end
+  end
 end
